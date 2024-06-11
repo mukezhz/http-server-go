92c33b1bc8a434bece2a5aa683e2fc57871158e2
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-06-11 06:52:02 +0545

    feat: send ok status code

--- app/server.go
+++ app/server.go
@@ -12,16 +12,23 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+
+	c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	_, err = c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
+		os.Exit(1)
+	}
+
+	c.Close()
 }
